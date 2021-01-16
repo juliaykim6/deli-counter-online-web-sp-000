77cc8521cd34f55ba4ec9b854995bb109c6b2a53
Julia Kim <juliaykim6@gmail.com>
2021-01-16 20:41:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,12 @@
 require 'pry'
 
 def line(array)
-  new_array = []
   new_array = array.map.with_index[1] do |name, index|
       "#{index}. #{name}"
   end
 
 if array.length > 0
-  puts "The line is currently: #{new_array.join" "}"
+  puts "The line is currently: #{new_array.join(" ")}"
 else
   puts "The line is currently empty."
 end
