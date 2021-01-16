67e820197aafd31c26f0e8cdc3bcdf886aad0f83
Julia Kim <juliaykim6@gmail.com>
2021-01-16 20:40:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(array)
   end
 
 if array.length > 0
-  puts "The line is currently: #{new_array}.join" "
+  puts "The line is currently: #{new_array.join" "}"
 else
   puts "The line is currently empty."
 end
