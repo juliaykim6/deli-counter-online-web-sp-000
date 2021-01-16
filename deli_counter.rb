4cd94d0d93614540f647167240a7758f4161afcf
Julia Kim <juliaykim6@gmail.com>
2021-01-16 20:56:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 
 def now_serving(array)
   if array.length > 0
-    puts "Currently serving #{array.shift}. "
+    puts "Currently serving #{array.shift}."
   else
     puts "There is nobody waiting to be served!"
 end
